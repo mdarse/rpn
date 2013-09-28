15710516d6b2539005f7bf8fb9300949009a7c73
Mathieu Darse <mattdarse@gmail.com>
2013-09-28 02:02:02 +0200

    Jison type declarations

--- lib/jison.d.ts
+++ lib/jison.d.ts
@@ -1,7 +1,21 @@
 declare module 'jison' {
+  interface JisonMap {
+    [s: number]: string
+  }
+  interface JisonLex {
+    rules: Array<JisonMap>
+  }
+  interface JisonBNF {
+    start: Array<JisonMap>;
+    input: Array<JisonMap>;
+    line: Array<JisonMap>;
+    exp: Array<JisonMap>;
+    operator: Array<JisonMap>;
+  }
+
   class Parser {
-    public yy: any;
-    constructor(configuration: Object);
+    yy: any;
+    constructor(configuration: {lex: JisonLex; bnf: JisonBNF});
     parse(input: string);
   }
 }
